f9261d6828a6d77608cc69cb67aac3331fd871a8
alicevik <cevikali83@yahoo.com>
2021-03-06 11:32:38 +0300

    api ile veri çekebildik.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import requests
 import json
+
+api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/"
+                           "latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=f4319da6-f95c-45e8-9c73-c5232e60f0d4")
+
+result = json.loads(api_request.content)  #çalışınca 200 çıkarsa başarılı anlamında
+
+print(result)
