45ee6098334a4cb381ca2ae967302ce56e081594
alikeremc <79354062+alikeremc@users.noreply.github.com>
2021-03-13 12:29:58 +0300

    Update main.py
    API Key Gizlendi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/"
-                           "latest?start=1&limit=10&convert=USD&CMC_PRO_API_KEY=f4319da6-f95c-45e8-9c73-c5232e60f0d4")
+                           "latest?start=1&limit=10&convert=USD&CMC_PRO_API_KEY=**********************************")
 
 result = json.loads(api_request.content)  #çalışınca 200 çıkarsa başarılı anlamında
 # print(result["status"]["total_count"])
